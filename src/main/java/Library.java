df813ce6f8755a46f29e8dcbee3de5efd99dad54
vova <wolodja@ukr.net>
2022-05-29 13:24:00 +0300

    first commit

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public interface Library {
+import java.util.LinkedList;
+
+public class Library {
+    private int size;
+    private final String libraryName;
+    private LinkedList<Book> bookList = new LinkedList<>();
+
+    public Library(String libraryName) {
+        this.libraryName = libraryName;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    private boolean isAddBook(Book books) {
+        return true;
+    }
+
+    private boolean isRemoveBook(Book nameBook) {
+        return true;
+    }
+
+    private boolean isRemoveBook(int nameBook) {
+        return true;
+    }
+
+    public Book getBook(){
+        return new Book();
+    }
+
+
 }
