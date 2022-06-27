6f2898c34d41c57a46ba80810987843b2e580a02
Hetsko Volodymyr <wolodjag@ukr.net>
2022-06-27 09:06:30 +0300

    update leptop db table

--- initDataBase.sql
+++
@@ -1,5 +0,0 @@
-
---INSERT INTO users(id, last_name, first_name, ssn, test1, test2, test3, test4, final_test, grade) VALUES (7216933567033945088,'Proselyte', 'Java', 'ssc', 2, 33.8, 44, 44, 22, 2.4, '90j0') ;
-
-
-
--- src/main/java/model/BookDescription.java
+++ src/main/java/model/BookDescription.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public class BookDescription {
     private long id;
-    private Book book;
     private Genre genre;
+    private Book book;
     private List<Book> numberOfCopies;
     private List<User> usersWhoRead;
 
