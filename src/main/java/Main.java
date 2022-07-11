03cd185dbb521f7a1cac5cce1aa537c87a5a27da
Hetsko Volodymyr <wolodjag@ukr.net>
2022-07-11 14:45:12 +0300

    delete dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,11 @@
 import model.Book;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
-
 public class Main {
     public static void main(String[] args) {
-
         SessionFactory factory = new Configuration()
-                .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Book.class)
+                .configure("hibernate.cfg.xml")
                 .buildSessionFactory();
     }
 }
