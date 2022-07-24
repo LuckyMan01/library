ff5e26bb7a6b15402050c98f2f8a663303d85ee7
Volodymyr Hetsko <55346306+LuckyMan01@users.noreply.github.com>
2022-07-24 18:48:30 +0300

    Delete src/main/java directory

--- src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Book book = new Book("Algoritm", "Anna Braun", "camedi", 1994);
-        Admin admin = new Admin();
-    }
-}
